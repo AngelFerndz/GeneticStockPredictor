b4eaafdf850079b69c9e6d73d33496d1f0e19acf
Fethore <mike.ferndz@outlook.com>
2020-08-22 16:43:06 -0400

    Adding genes to members

--- src/dev/angelf/geneticstockpredictor/Main.java
+++ src/dev/angelf/geneticstockpredictor/Main.java
@@ -1,9 +1,24 @@
 package dev.angelf.geneticstockpredictor;
 
+import dev.angelf.geneticstockpredictor.bot.Member;
+
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static ArrayList<Member> members;
 
+    public static void main(String[] args) {
+        initialize(100, 20);
+    }
+
+    private static void initialize(int amount, double money){
+        members = new ArrayList<>();
+        for(int i = 0; i < amount; i++){
+            Member member = new Member(money);
+            member.initialize();
+            members.add(member);
+        }
     }
 
 }
--- src/dev/angelf/geneticstockpredictor/bot/Member.java
+++ src/dev/angelf/geneticstockpredictor/bot/Member.java
@@ -4,30 +4,46 @@ import java.util.Random;
 
 public class Member {
 
+    int shares;
     double money;
     double boughtPrice;
 
     // Genes
     float stopLoss;
     float observationLength;
-    float holdTime;
+    float hold;
+    float avgRange;
 
-    public Member() {
-
+    public Member(double Money) {
+        shares = 0;
+        money = Money;
     }
 
     public void initialize(){
         Random random = new Random();
         stopLoss = random.nextFloat();
         observationLength = random.nextFloat();
-        holdTime = random.nextFloat();
+        hold = random.nextFloat();
+        avgRange = random.nextFloat();
     }
 
     public void mutate(){
         Random random = new Random();
-        stopLoss = stopLoss * random.nextFloat();
-        observationLength = observationLength * random.nextFloat();
-        holdTime = holdTime * random.nextFloat();
+        int i = random.nextInt(4);
+        switch (i){
+            case 0:
+                stopLoss = random.nextFloat();
+                break;
+            case 1:
+                observationLength = random.nextFloat();
+                break;
+            case 2:
+                hold = random.nextFloat();
+                break;
+            case 3:
+                avgRange = random.nextFloat();
+                break;
+        }
     }
 
     public void merge(Member member){
@@ -41,33 +57,63 @@ public class Member {
                 observationLength = member.observationLength;
                 break;
             case 2:
-                holdTime = member.holdTime;
+                hold = member.hold;
                 break;
         }
     }
 
     public void run(double[] prices){
         double currentPrice = prices[prices.length - 1];
-        int currentTime = prices.length - 1;
-        float observationTime = currentTime * observationLength;
-        double average = 0;
-        double averageRange = 0;
-
-        for (int i = 0; i > observationTime; i++) {
-
-        }
+        double average = getAverage(prices);
 
         if(currentPrice - boughtPrice > boughtPrice * stopLoss){
-            // sell at a loss
+            // sell at stop loss
+            sell(currentPrice);
         }
 
-        if (false) {
-            // sell at a gain
+        if (getAverageTop(average) < currentPrice && currentPrice > boughtPrice) {
+            // sell at a above average
+            sell(currentPrice);
         }
 
-        if(false){
-            // buy if lower than average
+        if(getAverageBottom(average) < currentPrice) {
+            buy(currentPrice);
         }
-
     }
+
+    private void sell(double currentPrice){
+        while (shares > 0) {
+            money += currentPrice;
+            shares--;
+        }
+    }
+
+    private void buy(double currentPrice){
+        while (money > currentPrice) {
+            money -= currentPrice;
+            shares++;
+        }
+    }
+
+    private double getAverage(double[] prices) {
+        int currentTime = prices.length - 1;
+        float observationTime = currentTime * observationLength;
+
+        // Calculate Average
+        double counter = 0;
+        for (int i = currentTime; i > 0; i++) {
+            counter += prices[i];
+        }
+
+        return counter / observationTime;
+    }
+
+    private double getAverageTop(double average){
+        return (average * avgRange) + average;
+    }
+
+    private double getAverageBottom(double average){
+        return (average * avgRange) - average;
+    }
+
 }
